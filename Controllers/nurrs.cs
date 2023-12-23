2f5540e86fd201aeef23eb0d32040381b6579408
gitisss <G0556790730@gmail.com>
2023-12-23 22:47:15 +0200

    first commit

---
+++ Controllers/nurrs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _231221.Controllers
+{
+    public class nurrs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
